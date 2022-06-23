001358affd090413c3072fcb90843b28702f6f9e
Thiago Sousa <80721628+ThgSousa@users.noreply.github.com>
2022-06-22 21:04:40 -0500

    Add files via upload

---
+++ Python/ConverterSegundosEmHoras.py
@@ -0,0 +1,12 @@
+
+x=0;
+print("Esse programa converte x segundos em horas");
+x = float(input("\n\nDigite os segundos: "))
+
+y = (x/60)/60;
+print(f"\n\n{x} segundos equivalem a {y} hora(s)\n\n");
+print("\n\nObrigado por usar nosso sistema!\n\n");
+
+
+
+
